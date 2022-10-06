801e5d20d2f2f55f8a8d4e5781cf51a56225aa52
Luis Vasquez <luizhito.lp.4ever@gmail.com>
2022-10-06 16:03:17 -0500

    Test deploy 4

--- decrypt.sh
+++ decrypt.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 
+echo $HOME
+pwd
 gpg --quiet --batch --yes --decrypt --passphrase="$PASSPHRASE" \
---output ./service-account.json ./k8s/service-account.json.gpg
+--output $HOME/service-account.json ./k8s/service-account.json.gpg
